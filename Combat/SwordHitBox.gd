fe20a96b65a27a00fadb4f8882ee9e935c26ce53
sliv90 <56741981+sliv90@users.noreply.github.com>
2021-08-19 16:00:07 +0200

    tutorial p14

--- Combat/SwordHitBox.gd
+++ Combat/SwordHitBox.gd
@@ -1,3 +1,3 @@
-extends Area2D
+extends "res://Combat/HitBox.gd"
 
 var knockback_vector = Vector2.ZERO
--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const FRICTION = 200
+export(int) var FRICTION = 200
 onready var stats = $Stats
 
 func _ready():
@@ -11,11 +11,11 @@ var knockback = Vector2.ZERO
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
-	knockback = move_and_slide(knockback)
-	
+	knockback = move_and_slide(knockback)	
 
 func _on_HurtBox_area_entered(area):
-	stats.health -= 1
-	if stats.health <= 0:
-		queue_free()
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_heatlh():
+	queue_free()
